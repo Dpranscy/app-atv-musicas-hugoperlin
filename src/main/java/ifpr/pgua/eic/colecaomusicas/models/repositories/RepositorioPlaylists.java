66dc52dc38671d007792cf08a5a67abb8b228a4e
Diego Prado <dprnasci32@gmail.com>
2023-09-08 14:51:03 -0300

    refatorando

---
+++ src/main/java/ifpr/pgua/eic/colecaomusicas/models/repositories/RepositorioPlaylists.java
@@ -0,0 +1,15 @@
+package ifpr.pgua.eic.colecaomusicas.models.repositories;
+
+import ifpr.pgua.eic.colecaomusicas.models.daos.PlaylistDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.hugoperlin.results.Resultado;
+
+import ifpr.pgua.eic.colecaomusicas.models.FabricaConexoes;
+import ifpr.pgua.eic.colecaomusicas.models.Playlist;
+
+public class RepositorioPlaylists {
+    
+}
